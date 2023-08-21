60745f8571a61d8e4ccaa8d628f7b88187f2147e
ErnieNishino <108204432+ErnieNishino@users.noreply.github.com>
2023-08-21 20:37:26 +0800

    Update binary_tree.py

--- binary_tree.py
+++ binary_tree.py
@@ -27,3 +27,20 @@ def isSameTree(p, q):
     if p.val != q.val:
         return False
     return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
+
+
+def isSymmetric(root):
+    def isMirror(node1, node2):
+        if node1 is None and node2 is None:
+            return True
+        if node1 is None or node2 is None:
+            return False
+        return (
+                node1.val == node2.val and
+                isMirror(node1.left, node2.right) and
+                isMirror(node1.right, node2.left)
+        )
+
+    if root is None:
+        return True
+    return isMirror(root.left, root.right)
